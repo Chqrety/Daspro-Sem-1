19e44849bb6a1490208eac4d44a5b2f79c8a12a9
Chqrety <fikriaverus23@gmail.com>
2025-03-06 08:06:42 +0700

    feat: add new exercise

--- exercise/pert-14/main.cpp
+++ exercise/pert-14/main.cpp
@@ -3,26 +3,27 @@ using namespace std;
 
 struct umr {
     string kab_kota;
-    string gaji;
+    int gaji;
 };
 
 int kurangRec(int a, int b);
-int maxRec(float arr[], int n);
+int maxRec(int arr[], int n);
+string maxGaji(umr arr[], int n);
 
 int main() {
-
     cout << "9 - 4 = " << kurangRec(9, 4) << endl;
-    float angka[5] = {78.2, 85, 5, 98, 3};
-    cout << "nilai max = " << maxRec(angka) << endl;
+
+    int arr[] = {2, 3, 5, 7, 9};
+    cout << "Nilai max = " << maxRec(arr, 5) << endl;
 
     umr jateng[3];
     jateng[0].kab_kota = "Surakarta";
-    jateng[0].gaji = "226970";
+    jateng[0].gaji = 22695370;
     jateng[1].kab_kota = "Semarang";
-    jateng[1].gaji = "32434369";
-    jateng[0].kab_kota = "Banjarnegara";
-    jateng[0].gaji = "2038005";
-    cout << "Gaji tertinggi = Rp " << maxRec(jateng, 3) << ",-" << endl;
+    jateng[1].gaji = 32434369;
+    // jateng[2].kab_kota = "Banjarnegara";
+    // jateng[2].gaji = 32254369;
+    cout << "Gaji tertinggi di Jawa Tengah adalah di " << maxGaji(jateng, 3) << endl;
 
     return 0;
 }
@@ -35,10 +36,18 @@ int kurangRec(int a, int b) {
     }
 }
 
-int maxRec(float arr[], int n) {
+int maxRec(int arr[], int n) {
     if (n == 1) {
         return arr[0];
     } else {
         return max(arr[n - 1], maxRec(arr, n - 1));
     }
+}
+
+int maxGaji(umr arr[], int n) {
+    if (n == 1) {
+        return arr[0].gaji;
+    }
+
+    return max(arr[n - 1].gaji, maxGaji(arr, n - 1));
 }
