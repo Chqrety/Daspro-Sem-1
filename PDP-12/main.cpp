c09f7ecf9e41665fc196625bab351d7c0f4978e8
Husnul Fikri Averus <94969229+Chqrety@users.noreply.github.com>
2025-12-14 19:54:42 +0700

    Update main.cpp

--- PDP-12/main.cpp
+++ PDP-12/main.cpp
@@ -7,28 +7,28 @@ int Max3(int a, int b, int c);
 int Max4(int a, int b, int c, int d);
 bool IsGanjil(int N);
 int Pangkat(int basis, int eksp);
-/*
-int SumOfN(ant N);
+int SumOfN(int N);
 int ProductOfN(int N);
 float Average(int N, int count);
 float AveSumOfN(int N);
 float AveProdOfN(int N);
 float C2F(float C);
-float C2K(Float C);
+float C2K(float C);
 float C2R(float C);
 bool IsPrima(int N);
-*/
+float medianArray(float arr[], int s);
+
 void cetakGaris();
 void cetakArray(float arr[], int s);
 
 int main() {
     // fungsi matematika
     cout << "Max2 --> 2 dan 5   = " << Max2(2, 5) << endl;
-    cout << "Max3 --> 2, 5, 1   = " << Max2(2, 5, 1) << endl;
-    cout << "Max4 --> 7, 2, 5, 1   = " << Max2(7, 2, 5, 1) << endl;
+    cout << "Max3 --> 2, 5, 1   = " << Max3(2, 5, 1) << endl;
+    cout << "Max4 --> 7, 2, 5, 1   = " << Max4(7, 2, 5, 1) << endl;
     cout << "isGanjil 93     = " << IsGanjil(93) << endl;
     cout << "isGanjil 1234   = " << IsGanjil(1234) << endl;
-    cout << "Pangkat(2,8)    = " << IsGanjil(2, 8) << endl;
+    cout << "Pangkat(2,8)    = " << Pangkat(2, 8) << endl;
     cout << "SumOfN(4)       = " << SumOfN(4) << endl;
     cout << "ProductOfN(4)   = " << ProductOfN(4) << endl;
     cout << "AveSumOfN(4)    = " << AveSumOfN(4) << endl;
@@ -41,8 +41,8 @@ int main() {
 
     cetakGaris();
 
-    float angka1[5] = [ 13, 41, 50, 1252, 2024 ];
-    float angka2[6] = [ 13, 19, 31, 50, 1252, 2024 ];
+    float angka1[5] = { 13, 41, 50, 1252, 2024 };
+    float angka2[6] = { 13, 19, 31, 50, 1252, 2024 };
 
     cout << endl << "Array angka1 : ";
     cetakArray(angka1, 5);
@@ -57,7 +57,7 @@ int main() {
 float medianArray(float arr[], int s) {
     if (IsGanjil(s) == 1) {
         int medianIndex = s / 2;
-        return arr[medianIndex]
+        return arr[medianIndex];
     } else {
         int med1 = (s / 2) - 1;
         int med2 = s / 2;
@@ -73,7 +73,7 @@ void cetakArray(float arr[], int s) {
     }
 }
 
-void2`` cetakGaris() {
+void cetakGaris() {
     cout << "=====================================";
 }
 
@@ -109,4 +109,47 @@ int Pangkat(int basis, int eksp) {
         hasil *= basis;
     }
     return hasil;
+}
+
+int SumOfN(int N) {
+    int sum = 0;
+    for (int i = 1; i <= N; i++)
+        sum += i;
+    return sum;
+}
+
+int ProductOfN(int N) {
+    int prod = 1;
+    for (int i = 1; i <= N; i++)
+        prod *= i;
+    return prod;
+}
+
+float AveSumOfN(int N) {
+    return (float)SumOfN(N) / N;
+}
+
+float AveProdOfN(int N) {
+    return (float)ProductOfN(N) / N;
+}
+
+float C2F(float C) {
+    return (C * 9 / 5) + 32;
+}
+
+float C2K(float C) {
+    return C + 273.15;
+}
+
+float C2R(float C) {
+    return C * 4 / 5;
+}
+
+bool IsPrima(int N) {
+    if (N <= 1) return false;
+    for (int i = 2; i * i <= N; i++) {
+        if (N % i == 0)
+            return false;
+    }
+    return true;
 }
